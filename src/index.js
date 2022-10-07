9e140426d2ab8c64141c0939d9fb02b859877d92
lea <jassilearg@gmail.com>
2022-10-07 16:41:43 -0300

    Correção bug login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const express = require('express');
-const { routes } = require('./routes');
+const routes = require('./routes');
 
 const app = express();
 
@@ -7,4 +8,5 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000);
+// app.listen(3000);
+app.listen(process.env.PORT);
