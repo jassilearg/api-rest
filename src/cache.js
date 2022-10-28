42093c81e7d628bb9978d2d1fb85df915943e2db
lea <jassilearg@gmail.com>
2022-10-28 14:42:06 -0300

    Cache com redis implementada

---
+++ src/cache.js
@@ -0,0 +1,37 @@
+const Redis = require("redis");
+
+class cache {
+
+    constructor() {
+        this.Redis = new Redis({
+            host: process.env.REDIS_HOST || "localhost",
+            port:  process.env.REDIS_PORT || "6379",
+            keyPrefix: "cache:"
+        })
+    }
+
+    async get(key) {
+        const value = await this.redis.get(key);
+
+        return value ? JSON.parse(value) : null;
+    }
+
+    set(key, value, timeExp) {
+        return this.redis(key, JSON.stringify(value), "EX", timeExp)
+    }
+
+    del(key) {
+        return this.redis.del(key);
+    }
+
+    async delPrefix(prefix) {
+        const keys = ( await this.redis.keys( `cache: ${prefix}:*` )).map((key) => 
+            key.replace("cache:", "")
+        );
+
+        return this.redis.del(keys);
+    }
+
+}
+
+module.exports = new cache();
